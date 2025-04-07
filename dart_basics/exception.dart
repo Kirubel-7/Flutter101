3593b4fdab297b377e83715139bd6ac49da347a3
Kirubel Esayas <107740002+Kirubel-7@users.noreply.github.com>
2025-04-08 00:59:06 +0300

    Exceptions with Setters and Getters

---
+++ dart_basics/exception.dart
@@ -0,0 +1,39 @@
+class NumberDivisionByZeroException implements Exception {
+  String toString() {
+    return 'Ouch! Division by zero!';
+  }
+}
+
+class Fraction {
+  int _numerator;
+  int _denominator;
+  Fraction(this._numerator, this._denominator) {
+    if (_denominator == 0) throw NumberDivisionByZeroException();
+  }
+  int get numerator => _numerator;
+  int get denominator => _denominator;
+  set denominator(int den) {
+    // if (den == 0) {
+    //   _denominator = 1;
+    // } else {
+    //   _denominator = den;
+    // }
+    den == 0 ? _denominator = 1 : _denominator = den;
+  }
+
+  @override
+  String toString() {
+    return '$_numerator/$_denominator';
+  }
+}
+
+void main() {
+  try {
+    Fraction fraction = Fraction(1, 2);
+    print(fraction);
+    fraction.denominator = 0;
+    print(fraction);
+  } on NumberDivisionByZeroException catch (ex) {
+    print(ex);
+  }
+}
